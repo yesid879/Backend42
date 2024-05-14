b84af38f11ba2dcb7bc791b1784aeed05b4555b3
yquintero.andes <yquintero.andes@gmail.com>
2024-05-14 18:44:43 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ const cors = require ('cors');
 
 // configuracion express  y  puerto
 const app = express();
-const port = 5000;
-app.use(express.json());
-
-//  aca  van las rutas de los  modulos 
-app.use('/api/clientes', require('../routes/RoutesCliente'));
+const port =  process.env.PORT || 5000;
 
 // enlazamos la conexion de la base de datos 
 conectarBD();
 app.use(cors());
+app.use(express.json());
+
+
+//  aca  van las rutas de los  modulos 
+app.use('/api/clientes', require('../routes/RoutesCliente'));
+
 
 //  se  configura el puerto que va tener  nuestro servidor 
 app.listen(port, () => console.log('Servidor Conectado http://localhost:5000'));
